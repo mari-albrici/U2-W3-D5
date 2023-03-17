e1f7792333a33bffb45d8bc238f8ab7f61ec527c
Marianna <marianna.albrici@gmail.com>
2023-03-17 11:48:42 +0100

    Random changes and addons

--- script.js
+++ script.js
@@ -6,55 +6,121 @@
 // }
 // })
 
+//FORM per creazione/modifica prodotti
+
+function preview() {
+	let inputPic = document.getElementById('inputPic');
+	let frame = document.getElementById('frame');
+	frame.src = inputPic.value;
+}
+function clearImage() {
+	let inputPic = document.getElementById('inputPic');
+	let frame = document.getElementById('frame');
+	inputPic.value = null;
+	frame.src = '';
+}
+
+const handleValidate = () => {
+	const form = document.querySelector('form');
+	form.classList.add('validated');
+};
+
+const URLParams = new URLSearchParams(window.location.search);
+const selectedId = URLParams.get('id');
+
+const endpoint = selectedId
+	? 'https://striveschool-api.herokuapp.com/api/product/' + selectedId
+	: 'https://striveschool-api.herokuapp.com/api/product/';
+const method = selectedId ? 'PUT' : 'POST';
+
+let productId = Math.floor(Math.random() * 100);
+
+let productName = document.getElementById('productName');
+let productDescription = document.getElementById('productDescription');
+let productPrice = document.getElementById('productPrice');
+let productImage = document.getElementById('frame');
+let productBrand = document.getElementById('productBrand');
+
+const newProduct = {
+	_id: productId,
+	name: productName.value,
+	description: productDescription.value,
+	price: productPrice.value,
+	brand: productBrand.value,
+	imageUrl: productImage.src,
+};
+
 const handleSubmit = async (event) => {
 	event.preventDefault();
-
-	// creazione dell'oggetto che invieremo come payload
-	// N.B. la creazione di newAppointment viene fatta ad ogni submit del form
-	const newProduct = {
-		name: document.getElementById('name').value,
-		description: document.getElementById('description').value,
-		price: document.getElementById('price').value,
-		time: document.getElementById('time').value,
-	};
-
-	console.log('HERE ON SUBMIT', newAppointment);
+	console.log('HERE ON SUBMIT', newProduct);
 
 	try {
-		// attivo lo stato di caricamento
-		isLoading(true);
-
-		// qui l'endpoint dipende da come siamo arrivati su questa pagina, se per creazione sarà solo l'url normale, se per modifica avrà anche l'id
-		// questo è deciso dal ternary operator alla creazione della variabile "endpoint" in alto
 		const resp = await fetch(endpoint, {
-			method, // uguale a scrivere method: method,
-			body: JSON.stringify(newAppointment), // è fondamentale fare la stringhifizzazione dell'oggetto nativo o invieremo "[object Object]"
-			// un header in particolare è importantissimo, il Content-Type, per specificare il formato di invio, altrimenti non verrà riconosciuto dal server
-			// l'Authorization header serve in caso di API che richiedono autenticazione tramite una API Key
+			method,
+			body: JSON.stringify(newProduct),
 			headers: {
-				// "Authorization" : "Bearer [YOUR API KEY]", // metodo di autenticazione con API Key standard
+				Authorization:
+					'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MGNkYWY4MWI0MjAwMTM5YjI3YjgiLCJpYXQiOjE2NzkwMzU2MTAsImV4cCI6MTY4MDI0NTIxMH0.8xfKaqz9fVZRG9kGb-jEnd3VRMa0XZB1q9e_wwtpgCI',
 				'Content-Type': 'application/json',
 			},
 		});
 
 		if (resp.ok) {
-			const newAppObj = await resp.json();
-			// Aspettiamo il valore di newAppObj per estrarre un'informazione nuova generata dal server ossia l'_id
-
-			// in base a se siamo qui per creazione o modifica creeremo il messaggio più appropriato alla fine della richiesta
+			const newProductObj = await resp.json();
 			if (selectedId) {
-				alert("Risorsa con l'id " + newAppObj._id + ', modificata con successo');
+				alert("Risorsa con l'id " + newProductObj._id + ', modificata con successo');
 			} else {
-				alert("Risorsa con l'id " + newAppObj._id + ', creata con successo');
+				alert("Risorsa con l'id " + newProductObj._id + ', creata con successo');
 			}
 		} else {
 			throw new Error('La richiesta non è andata a buon fine');
 		}
 	} catch (error) {
 		alert(error);
-	} finally {
-		// spengo il loader di caricamento sia quando la richiesta va a buon fine, sia quando abbiamo un errore e si attiva il catch
-		// poco prima di uscire dall'esecuzione del contesto, il metodo finally si attiva a sua volta
-		isLoading(false);
 	}
 };
+
+const handleDelete = async () => {
+	const hasAccepted = confirm('Sei sicuro di voler eliminare il prodotto?');
+
+	// se accetta procediamo all'effettiva rimozione
+	if (hasAccepted) {
+		try {
+			const resp = await fetch(endpoint, { method: 'DELETE' });
+			const deletedProduct = await resp.json();
+
+			alert('Hai eliminato il prodotto ' + deletedProduct.name);
+			window.location.assign('./backoffice.html');
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
+
+let loadProducts = async () => {
+	try {
+		let loadProducts = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
+			headers: {
+				Authorization:
+					'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MGNkYWY4MWI0MjAwMTM5YjI3YjgiLCJpYXQiOjE2NzkwMzU2MTAsImV4cCI6MTY4MDI0NTIxMH0.8xfKaqz9fVZRG9kGb-jEnd3VRMa0XZB1q9e_wwtpgCI',
+			},
+		});
+		let products = await loadProducts.json();
+
+		let cards = document.querySelectorAll('.card');
+		cards.forEach((card, index) => {
+			card.innerHTML = `
+            <img src="${products[index].imageUrl}" class="card-img-top" alt="none">
+            <div class="card-body">
+              <h5 class="card-title">${products[index].name}</h5>
+              <p class="card-text">${products[index].description}</p>
+              <p class="card-text">€${products[index].price}</p>
+              <a href="#" class="btn btn-primary">View</a>
+            </div>
+          </div>`;
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+loadProducts();
